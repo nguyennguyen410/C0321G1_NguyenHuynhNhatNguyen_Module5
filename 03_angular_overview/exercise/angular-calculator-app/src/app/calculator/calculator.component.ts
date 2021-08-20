6f6e92245dfd8ce4e672e4acbb8e7bf43ab43f52
nguyennguyen410 <nguyen.nhn89@gmail.com>
2021-08-20 23:16:09 +0700

    03_angular_overview

---
+++ 03_angular_overview/exercise/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  num1:number;
+  num2:number;
+  sum:number;
+  sub:number;
+  multi:number;
+  division:number;
+
+  calculatorNumber(): void {
+    this.sum = Number(this.num1) + Number(this.num2);
+    this.sub = Number(this.num1) - Number(this.num2);
+    this.multi = Number(this.num1) * Number(this.num2);
+    this.division = Number(this.num1) / Number(this.num2);
+  }
+
+}
